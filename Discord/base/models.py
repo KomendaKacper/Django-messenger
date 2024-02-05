bf025b831c3a5f6a05ace0f38804b6e90af26562
Kacper Komenda <komenda@student.agh.edu.pl>
2024-02-05 19:21:13 +0100

    aaa

--- Discord/base/models.py
+++ Discord/base/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Room(models.Model):
+    #host = 
+    #topic = 
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True) #Może powstać bez opisu
+    #participants =
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True) #zapisuje tylko pierwsze stworzenie
+
+    def __str__(self):
+        return self.name
