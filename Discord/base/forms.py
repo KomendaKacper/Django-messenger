cf715da53216137847038d61fc91c803d6acbf87
Kacper Komenda <komenda@student.agh.edu.pl>
2024-02-06 23:51:56 +0100

    Edit

---
+++ Discord/base/forms.py
@@ -0,0 +1,7 @@
+from django.forms import ModelForm
+from .models import Room
+
+class RoomForm(ModelForm):
+    class Meta:
+        model = Room #model = obiekt który chcesz stworzyć
+        fields = '__all__'
