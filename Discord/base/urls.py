62b18cb03426455da72dbec09e3329e2c9e39750
Kacper Komenda <komenda@student.agh.edu.pl>
2024-02-09 00:22:40 +0100

    ajjj

--- Discord/base/urls.py
+++ Discord/base/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('create-room/', views.createRoom, name="create-room"),
     path('update-room/<str:pk>/', views.updateRoom, name="update-room"),
     path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room"),
-    path('update-room', views.updateRoom, name='updateRoom')
 ]
--- Discord/base/views.py
+++ Discord/base/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
 # Create your views here.
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 
 def home (request):
-    rooms = Room.objects.all()
-    context  = {'rooms': rooms}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(topic__name__contains=q)
+    topics = Topic.objects.all()
+
+    context  = {'rooms': rooms, 'topics':topics}
     return render(request, 'base/home.html', context)
 
 def room (request, pk):
