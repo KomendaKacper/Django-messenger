bd56e2de1219ac8fdfb5d24199ee4332eb178546
Kacper Komenda <komenda@student.agh.edu.pl>
2024-02-12 00:00:00 +0100

    3.53

--- Discord/base/views.py
+++ Discord/base/views.py
@@ -60,7 +60,7 @@ def home (request):
                                 )
     topics = Topic.objects.all()
     room_count = rooms.count() #Wbudowana metoda
-    room_messages = Message.objects.all()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
 
     context  = {'rooms': rooms, 'topics':topics,
@@ -82,6 +82,16 @@ def room (request, pk):
         return redirect('room', pk=room.id)
     context = {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'base/room.html', context)
+
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all() #Wszystkie dzieci usera
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user': user, 'rooms': rooms,
+                'room_messages': room_messages,
+                'topics': topics}
+    return render(request, 'base/profile.html', context)
 
 @login_required(login_url='login')
 def createRoom(request):
