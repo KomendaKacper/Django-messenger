2c26e6e56be8d6b8444e31d238e77769424e6f48
KomendaKacper <komenda@student.agh.edu.pl>
2024-02-23 18:08:00 +0100

    updateUser

--- Discord/base/forms.py
+++ Discord/base/forms.py
@@ -1,8 +1,14 @@
 from django.forms import ModelForm
 from .models import Room
+from django.contrib.auth.models import User
 
 class RoomForm(ModelForm):
     class Meta:
         model = Room #model = obiekt który chcesz stworzyć
         fields = '__all__'
         exclude = ['host', 'participants']
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
